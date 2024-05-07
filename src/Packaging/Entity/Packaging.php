87d7fcef49d69422ba80e197fc5071482cd1b5f9
Mateusz Zalewski <zaleslaw@gmail.com>
2024-05-07 21:37:26 +0200

    New Packaging entity

---
+++ src/Packaging/Entity/Packaging.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Packaging\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Resource\Model\ResourceInterface;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'app_packaging')]
+class Packaging implements ResourceInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    #[ORM\Column(type: 'string')]
+    private string $code;
+
+    #[ORM\Column(type: 'integer')]
+    private int $price;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): void
+    {
+        $this->code = $code;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+}
